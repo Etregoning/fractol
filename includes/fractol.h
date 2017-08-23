83a533cf805f1219ce6b614b769cf203c443fb72
Etregoning <etregoni@e1z2r4p27.42.us.org>
2017-08-23 15:34:08 -0700

    Updated main.c and fractol.h

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,8 +6,16 @@
 /*   By: etregoni <etregoni@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 18:51:28 by etregoni          #+#    #+#             */
-/*   Updated: 2017/08/22 18:51:36 by etregoni         ###   ########.fr       */
+/*   Updated: 2017/08/23 15:32:15 by etregoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRACTOL_H
+# define FRACTOL_H
 
+# include "mlx.h"
+# include "libft.h"
+# include <math.h>
+# include <stdio.h>
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: etregoni <etregoni@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/22 18:51:46 by etregoni          #+#    #+#             */
-/*   Updated: 2017/08/23 14:17:34 by etregoni         ###   ########.fr       */
+/*   Updated: 2017/08/23 14:27:20 by etregoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	main(int ac, char **av)
 {
+	void	*mlx;
+
 	if (ac != 2)
 	{
 		ft_error("Usage: ./fractol <fractal name>\n
 				Available fractals: mandelbrot, julia");
 	}
-
+	mlx = mlx_init();
 }
